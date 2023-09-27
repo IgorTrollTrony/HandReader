a65f34e2514bfa97a7534d36d46973153a814baa
WaydsonTeodoro-hub <teodororgamer@gmail.com>
2023-09-27 19:35:09 +0000

    Achar tags e colocar niveis nas de abertura

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-print("teste")
-print("teste2")
-print("teste3")
+import re
+
+entrada_html = """<html> <head> <title> Compiladores </title> </head><body> <p style="color:red;background:blue;" id="abc"> Unipinhal </p> <br> </body></html>"""
+
+#/? pode ou ter /
+#\w+ um ou mais caracteres de palavra
+tags = re.findall(r'<(/?\w+)([^>]*)>', entrada_html)
+nivel = 0
+for tag, atributos in tags:
+  if '/' not in tag:
+    print(f"Tag de abertura: {tag},Nivel : {nivel}")
+    nivel += 1
+  else:
+    print(f"Tag de fechamento: {tag}")
