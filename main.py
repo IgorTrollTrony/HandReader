a7356370c69e3ab51f8f5ea791c2a1de9e7fddc9
WaydsonTeodoro-hub <teodororgamer@gmail.com>
2023-09-27 19:59:52 +0000

    Tags de fechamento com nivel

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 import re
 
-entrada_html = """<html> <head> <title> Compiladores </title> </head><body> <p style="color:red;background:blue;" id="abc"> Unipinhal </p> <br> </body></html>"""
+entrada_html = """<html lang="pt-br">
+<head>
+    <title>Exemplo de HTML com Duas Divs</title>
+</head>
+<body>
+    <div>
+        <h1>Div 1</h1>
+        <p><span></span></p>
+    </div>
+    <div>
+        <h1>Div 2</h1>
+        <p>Esta é a segunda div.</p>
+    </div>
+</body>
+</html>"""
 
 #/? pode ou ter /
 #\w+ um ou mais caracteres de palavra
 tags = re.findall(r'<(/?\w+)([^>]*)>', entrada_html)
+
+tags_abertura = []
 nivel = 0
 for tag, atributos in tags:
   if '/' not in tag:
-    print(f"Tag de abertura: {tag},Nivel : {nivel}")
-    nivel += 1
+    print(f"Tag de abertura: {tag}, Nível: {nivel}")
+    tags_abertura.append(tag)
+    nivel += 1  # Aumenta o nível
   else:
-    print(f"Tag de fechamento: {tag}")
+     tag_fechamento = tag[1:]  
+     if tags_abertura and tags_abertura[-1] == tag_fechamento:
+        nivel -= 1
+        print(f"Tag de fechamento: {tag_fechamento}, Nível: {nivel}")
+        tags_abertura.pop()  
