1925451baf28d91b3932de84f17917418029f725
IgorTrollTrony <144748049+IgorTrollTrony@users.noreply.github.com>
2023-09-29 00:04:22 +0000

    Correção de tags que não possuem fechamento.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ entrada_html = """<html lang="pt-br">
 </head>
 <body>
     <div>
+      <br>
         <h1>Div 1</h1>
         <p><span></span></p>
     </div>
@@ -13,6 +14,7 @@ entrada_html = """<html lang="pt-br">
         <h1>Div 2</h1>
         <p>Esta é a segunda div.</p>
     </div>
+    
 </body>
 </html>"""
 
@@ -23,13 +25,20 @@ tags = re.findall(r'<(/?\w+)([^>]*)>', entrada_html)
 tags_abertura = []
 nivel = 0
 for tag, atributos in tags:
-  if '/' not in tag:
-    print(f"Tag de abertura: {tag}, Nível: {nivel}")
-    tags_abertura.append(tag)
-    nivel += 1  # Aumenta o nível
-  else:
-     tag_fechamento = tag[1:]  
-     if tags_abertura and tags_abertura[-1] == tag_fechamento:
+    if '/' not in tag:
+      if not tag.endswith('/'):
+        print(f"Tag de abertura: {tag}, Nível: {nivel}")
+        if not tag.lower() in['br','img', 'h']: 
+          tags_abertura.append(tag)
+          nivel += 1  # Aumenta o nível
+    else:
+      tag_fechamento = tag[1:]
+      if tags_abertura and tags_abertura[-1] == tag_fechamento:
         nivel -= 1
         print(f"Tag de fechamento: {tag_fechamento}, Nível: {nivel}")
-        tags_abertura.pop()  
+        tags_abertura.pop()
+# Imprime as tags restantes. #
+while tags_abertura:
+  tag_aberta = tags_abertura.pop()
+  print(f"Tag de abertura não correspondente: {tag_aberta}, Nível: {nivel}")
+  nivel -= 1
