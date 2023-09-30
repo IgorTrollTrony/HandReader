074ee8d81a08c710e49d862affaff1e493e70c21
IgorTrollTrony <144748049+IgorTrollTrony@users.noreply.github.com>
2023-09-30 03:21:27 +0000

    Organizaçao da saida de resultados

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ entrada_html = """<html lang="pt-br">
       <br>
         <h1>Div 1</h1>
         <p><span></span></p>
+        
     </div>
     <div>
         <h1>Div 2</h1>
@@ -23,22 +24,23 @@ entrada_html = """<html lang="pt-br">
 tags = re.findall(r'<(/?\w+)([^>]*)>', entrada_html)
 
 tags_abertura = []
+tags_fechamento = []
 nivel = 0
 for tag, atributos in tags:
-    if '/' not in tag:
-      if not tag.endswith('/'):
-        print(f"Tag de abertura: {tag}, Nível: {nivel}")
-        if not tag.lower() in['br','img', 'h']: 
-          tags_abertura.append(tag)
-          nivel += 1  # Aumenta o nível
-    else:
-      tag_fechamento = tag[1:]
-      if tags_abertura and tags_abertura[-1] == tag_fechamento:
-        nivel -= 1
-        print(f"Tag de fechamento: {tag_fechamento}, Nível: {nivel}")
-        tags_abertura.pop()
-# Imprime as tags restantes. #
-while tags_abertura:
-  tag_aberta = tags_abertura.pop()
-  print(f"Tag de abertura não correspondente: {tag_aberta}, Nível: {nivel}")
-  nivel -= 1
+  if '/' not in tag:
+    if not tag.endswith('/'):
+      print(f"Tag de abertura: {tag}, Nível: {nivel}")
+      if not tag.lower() in ['br', 'img', 'h']:
+        tags_abertura.append(tag)
+        nivel += 1
+  else:
+    tag_fechamento = tag[1:]
+    if tags_abertura and tags_abertura[-1] == tag_fechamento:
+      nivel -= 1
+      tags_fechamento.append("Tag de fechamento : "+str(tag_fechamento) +" "+"Nivel : "+ str(nivel))
+      tags_abertura.pop()
+print("")
+for tag in tags_fechamento:
+  print(tag)
+
+#Organizaçao da saida de resultados
